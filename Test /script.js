f62f11ebc0f7f072adf40fb4064ecc06663ddceb
nruffin95 <nruffin95@gmail.com>
2022-04-15 02:32:37 -0500

    Updated html

--- Test /script.js
+++ Test /script.js
@@ -1,34 +1,3 @@
-const questionContainer = document.getElementById('question-container')
-const startButton = document.getElementById('start-btn')
-const questionElement = document.getElementById('question')
-const questionButtons = document.getElementById('answer-btn')
-
-
-startButton.addEventListener('click', startGame)
-
-  function startGame() {
-    startButton.classList.add('hide')
-    questionContainer.classList.remove('hide')
-    showQuestion()
-  }
-
-function showQuestion(question){
-  questionElement.innerText = questions.question
+* {
+  bo
 }
-
-function selectAnswer(){
-
-}
-
-
-const questions = [
-  {
-    question: 'What is your favorite youtuber'
-  
-  }
-]
-
-
-
-
-// const questions = {question:"Who Is Your Favorite Youtuber", answers:"Nick"};
