e01dffcd4d378bb525503234a842a9e4ced0e4bb
nruffin95 <nruffin95@gmail.com>
2022-04-14 22:22:20 -0500

    Added Button functionality

--- Test /script.js
+++ Test /script.js
@@ -1,106 +1,34 @@
 const questionContainer = document.getElementById('question-container')
-const quizContainer = document.getElementById('start-btn')
+const startButton = document.getElementById('start-btn')
 const questionElement = document.getElementById('question')
-const buttonElement = document.getElementById('answer-btn')
+const questionButtons = document.getElementById('answer-btn')
 
-const btn1 = document.getElementById('btn1')
-const btn2 = document.getElementById('btn2')
-const btn3 = document.getElementById('btn3')
-const btn4 = document.getElementById('btn4')
 
-function removeQuestions(){
-  questionElement.remove();
-  buttonElement.remove()
+startButton.addEventListener('click', startGame)
+
+  function startGame() {
+    startButton.classList.add('hide')
+    questionContainer.classList.remove('hide')
+    showQuestion()
+  }
+
+function showQuestion(question){
+  questionElement.innerText = questions.question
 }
 
-document.getElementById("start-btn").onclick = function () {
-    quizContainer.remove();
+function selectAnswer(){
 
-    var element = document.getElementById("question-container");
-    element.classList.remove("hide");
-    showQuestions1()
-};
-
-function showQuestions1(){
-  questionElement.textContent = 'Commonly Used data types DO NOT include?'
-  btn1.textContent = 'Strings'
-  btn2.textContent = 'booleans'
-  btn3.textContent = 'alerts'
-  btn4.textContent = 'numbers'
-
-  btn1.addEventListener("click", myFunction);
-    function myFunction(){
-    alert('Correct')
-    removeQuestions();
-  }
-
-  btn2.addEventListener("click", myFunction2);
-    function myFunction2(){
-    alert('Incorrect')
-    removeQuestions()
-  }
-
-  btn3.addEventListener("click", myFunction3);
-    function myFunction3(){
-    alert('Incorrect')
-    removeQuestions()
-  }
-
-  btn4.addEventListener("click", myFunction4);
-    function myFunction4(){
-    alert('Incorrect')
-    removeQuestions()
-  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 const questions = [
-    {
-      question: 'Commonly Used data types DO NOT include?',
-      answers: [
-        {text: 'Strings', wrong: false},
-        {text: 'booleans', wrong: false},
-        {text: 'alerts', correct: true},
-        {text: 'numbers', wrong: false},
-      ]
-    },
-
-    {
-        question: 'Who is the best youtuber?',
-        answers: [
-          {text: 'Nick', correct: true},
-          {text: 'Wilson', wrong: false},
-          {text: 'Mark', wrong: false},
-          {text: 'Will', wrong: false},
-        ]
-      },
-    
-      {
-        question: 'Who has the best football teams?',
-        answers: [
-          {text: 'Chiefs', wrong: false},
-          {text: 'Steelers', wrong: false},
-          {text: 'Rams', correct: true},
-          {text: 'Dolphins', wrong: false},
-        ]
-      },  
+  {
+    question: 'What is your favorite youtuber'
+  
+  }
 ]
+
+
+
+
+// const questions = {question:"Who Is Your Favorite Youtuber", answers:"Nick"};
