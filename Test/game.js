cdbde568f2444198032c7e24d1ce3a7d4bc48e1c
nruffin95 <nruffin95@gmail.com>
2022-04-15 04:25:46 -0500

    Fixed End Game Return Screen

--- Test/game.js
+++ Test/game.js
@@ -59,7 +59,7 @@ getNewQuestion = () => {
     if(availableQuestions.length ===0 || questionCounter > MAX_QUESTIONS){
         localStorage.setItem('mostRecentScore', score)
 
-        return window.location.assign('/Users/nick/Desktop/githubtest/Test /end.html')
+        return window.location.assign('end.html')
     }
 
     questionCounter++
